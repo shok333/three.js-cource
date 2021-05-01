c949d94ab4f18e79327c8241a8034b4c1cbf6e90
shok3333 <shok-dimon@mail.ru>
2021-05-01 14:09:48 +0300

    1 - hello world

---
+++ 1 - hello world/src/ThreeContainer.js
@@ -0,0 +1,21 @@
+import {memo, useRef, useEffect} from "react";
+
+import Three from "./Three";
+
+const NativeRenderer = () => {
+  const threeRef = useRef(); //Используется для обращения к контейнеру для canvas
+  const three = useRef(); //Служит для определения, создан ли объект, чтобы не создавать повторный
+
+  // Создание объекта класса Three, предназначенного для работы с three.js
+  useEffect(() => {
+    if (!three.current) {
+      three.current = new Three({
+        canvasContainer: threeRef.current,
+      });
+    }
+  }, []);
+
+  return <div className="container" ref={threeRef} />;
+};
+
+export default memo(NativeRenderer);
